61ce870ea7dc7b4081c3e2b534974253d9804117
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-09-19 18:07:19 +0300

    Create 9.TheatrePromotions.js

---
+++ Basic Syntax, Conditional Statements and Loops/9.TheatrePromotions.js
@@ -0,0 +1,45 @@
+function solve(day, age) {
+  price = 0;
+  if (0 <= age & age < 18) {
+    switch (day) {
+      case "Weekday":
+        price = +12;
+        break;
+      case "Weekend":
+        price = +15;
+        break;
+      case "Holiday":
+        price += 5;
+        break;
+    }
+    }else if (18 < age & age <= 64) {
+    switch (day) {
+      case "Weekday":
+        price = +18;
+        break;
+      case "Weekend":
+        price = +20;
+        break;
+      case "Holiday":
+        price += 12;
+        break;
+    }
+  }else if (64 < age & age <= 122) {
+    switch (day) {
+      case "Weekday":
+        price = +12;
+        break;
+      case "Weekend":
+        price = +15;
+        break;
+      case "Holiday":
+        price += 10;
+        break;
+    }
+  }
+  if(price > 0){
+    console.log(`${price}$`)
+  }else{
+    console.log("Error!")
+  }
+}
