f2eee927ba463dcd2b30208cb88ddab74cad5cc1
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-09-22 12:19:54 +0300

    Update 9.Login.js

--- Basic Syntax, Conditional Statements and Loops Exercise/9.Login.js
+++ Basic Syntax, Conditional Statements and Loops Exercise/9.Login.js
@@ -1,34 +1,16 @@
-function solve (input) {
- 
-        let index = 0;
-        let username = input[index++]; 
-        let password = input[index++];
-        let counter = 0;
- 
-        while (true) {
- 
-            let invertedIncomingpassword ="";
-            for (let i = password.length - 1; i >= 0; i--) {
-                    let part = password[i];
- 
-                    invertedIncomingpassword = invertedIncomingpassword + part
- 
-                }   
- 
-                counter++;
-            if (username !== invertedIncomingpassword) {
- 
-                if (counter === 4) {
-                    console.log(`User ${username} blocked!`);
-                    break;
-                }
-                console.log("Incorrect password. Try again.");
- 
- 
-            } else {
-                console.log(`User ${username} logged in.`);
-                break;
-            }
-            password = input[index++];
-        }
+function solve(arr) {
+  let username = arr.shift();
+  let pass = username.split("").reverse().join("");
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === pass) {
+      console.log(`User ${username} logged in.`);
+    } else {
+      if (i == 3) {
+        console.log(`User ${username} blocked!`);
+        break;
+      }
+      console.log(`Incorrect password. Try again.`);
     }
+  }
+}
+solve(["Acer", "login", "go", "let me in", "recA"]);
