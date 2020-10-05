0580bd470ae274f04ad67acac3604557a04df938
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-10-05 17:33:47 +0300

    Create 8.MagicSum.js

---
+++ Arrays Exercise/8.MagicSum.js
@@ -0,0 +1,12 @@
+function solve(arr, num) {
+  for (let i = 0; i < arr.length; i++) {
+
+    for (let j = i + 1; j < arr.length; j++) {
+      let sum = arr[i] + arr[j]
+      if(sum === num){
+        console.log(`${arr[i]} ${arr[j]}`)
+      }
+    }
+  }
+}
+solve([14, 20, 60, 13, 7, 19, 8], 27);
