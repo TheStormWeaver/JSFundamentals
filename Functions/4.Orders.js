a4895cc2deed8bc9201d41d32ef2e81083de966a
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-10-10 16:56:53 +0300

    Create 4.Orders.js

---
+++ Functions/4.Orders.js
@@ -0,0 +1,16 @@
+function solve(product, quantity) {
+  let result = 0;
+  for (let i = 0; i < quantity; i++) {
+    if (product == "coffe") {
+      result += 1.5;
+    } else if (product == "water") {
+      result += 1.0;
+    } else if (product == "coke") {
+      result += 1.4;
+    } else if (product == "snacks") {
+      result += 2.0;
+    }
+  }
+  console.log(result)
+}
+solve("coffe", 2);
