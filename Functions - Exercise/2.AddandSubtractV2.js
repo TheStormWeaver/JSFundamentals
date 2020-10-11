660419241621e64de06be5d53da6f98b95a225b4
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-10-11 15:12:53 +0300

    Create 2.AddandSubtractV2.js

---
+++ Functions - Exercise/2.AddandSubtractV2.js
@@ -0,0 +1,8 @@
+function AddandSubtract(numOne, numTwo, numThree) {
+ function sum(a,b){
+   return a + b
+ }
+ let result = sum(numOne, numTwo)
+ return result - numThree
+}
+console.log(AddandSubtract(23,6,10));
