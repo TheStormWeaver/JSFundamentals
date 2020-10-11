2abfeebea6293f7c54f9ac479182edadad23bc91
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-10-11 14:06:42 +0300

    Create 1.SmallestofThreeNumbersV2.js

---
+++ Functions - Exercise/1.SmallestofThreeNumbersV2.js
@@ -0,0 +1,14 @@
+function SmallestofThree(numOne, numTwo, numThree) {
+  let smallest = Number.MAX_SAFE_INTEGER;
+   smallest = smaller(numOne, smallest)
+   smallest = smaller(numTwo, smallest)
+   smallest = smaller(numThree, smallest)
+  function smaller(n, small){
+    if(n < small){
+      small = n
+    }
+    return small
+  }
+  return smallest
+}
+console.log(SmallestofThree(2,5,3));
