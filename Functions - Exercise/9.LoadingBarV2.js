a091eddfa61713cf63456f961593509038bf8eb8
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-10-11 17:17:11 +0300

    Create 9.LoadingBarV2.js

---
+++ Functions - Exercise/9.LoadingBarV2.js
@@ -0,0 +1,16 @@
+function loadingBar(n) {
+  let procentnum = n /10
+  let procentloader = ".........."
+  let procentarr = procentloader.split('')
+  if(n == 100){
+    console.log(`100% Complete!`)
+    console.log(`[%%%%%%%%%%]`)
+  }else{
+    for (let i = 0; i < procentnum; i++) { 
+      procentarr.pop()
+      procentarr.unshift("%")   
+    }
+    console.log(`${n}% [${procentarr.join('')}]`)
+    console.log(`Still loading...`)
+  }
+}
