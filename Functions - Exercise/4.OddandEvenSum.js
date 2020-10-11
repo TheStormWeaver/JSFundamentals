2668970de0f558b63709492eca93ce6f39e06a72
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-10-11 15:34:20 +0300

    Create 4.OddandEvenSum.js

---
+++ Functions - Exercise/4.OddandEvenSum.js
@@ -0,0 +1,12 @@
+function OddandEvenSum(a, b) {
+  let firstchar = a.charCodeAt();
+  let secondchar = b.charCodeAt();
+  let start = Math.min(firstchar, secondchar)
+  let end = Math.max(firstchar, secondchar)
+  let line = "";
+  for (let i = start + 1; i < end; i++) {
+    line += String.fromCharCode(i) + " ";
+  }
+  return line;
+}
+console.log(OddandEvenSum("a", "d"));
