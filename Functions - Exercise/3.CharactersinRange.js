9f2ff6ad7b5a734f6532ed9ceb0ca7c366dffd84
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-10-11 15:34:53 +0300

    Update 3.CharactersinRange.js

--- Functions - Exercise/3.CharactersinRange.js
+++ Functions - Exercise/3.CharactersinRange.js
@@ -1,8 +1,8 @@
 function charinRange(a, b) {
   let firstchar = a.charCodeAt();
   let secondchar = b.charCodeAt();
-  let start = firstchar < secondchar ? firstchar : secondchar;
-  let end = firstchar > secondchar ? firstchar : secondchar;
+  let start = Math.min(firstchar, secondchar)
+  let end = Math.max(firstchar, secondchar)
   let line = "";
   for (let i = start + 1; i < end; i++) {
     line += String.fromCharCode(i) + " ";
