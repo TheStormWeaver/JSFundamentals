59942923b9e8ac9b02cb1f1956e8dcd34ae06145
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-10-17 15:29:58 +0300

    Update 3.FirstandLastKNumbers.js

--- Arrays Advanced/3.FirstandLastKNumbers.js
+++ Arrays Advanced/3.FirstandLastKNumbers.js
@@ -1,6 +1,7 @@
 function firstlastk(arr) {
   let k = arr.shift()
-  console.log(arr.slice(0, k))
-  console.log(arr.slice(arr.length - k, k + 1))
+  console.log(arr.slice(0, k).join(" "))
+  console.log(arr.slice(arr.length - k, k + 1).join(" "))
 }
 firstlastk([2, 7, 8, 9]);
+
