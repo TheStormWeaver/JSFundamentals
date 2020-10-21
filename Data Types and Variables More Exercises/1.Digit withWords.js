e77dba59999ed59ed5837d86eb9efb3e7ecbb2b7
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-10-21 13:58:12 +0300

    Create 1.Digit withWords.js

---
+++ Data Types and Variables More Exercises/1.Digit withWords.js
@@ -0,0 +1,37 @@
+function Digits(num) {
+  let finalsum;
+  switch (num) {
+    case "zero":
+      finalsum = 0;
+      break;
+    case "one":
+      finalsum = 1;
+      break;
+    case "two":
+      finalsum = 2;
+      break;
+    case "three":
+      finalsum = 3;
+      break;
+    case "four":
+      finalsum = 4;
+      break;
+    case "five":
+      finalsum = 5;
+      break;
+    case "six":
+      finalsum = 6;
+      break;
+    case "seven":
+      finalsum = 7;
+      break;
+    case "eight":
+      finalsum = 8;
+      break;
+    case "nine":
+      finalsum = 9;
+      break;
+  }
+  console.log(finalsum)
+}
+Digits("nine");
