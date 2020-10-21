327a3d028507fb093cf9e95d24c22a70d458cf4a
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-10-21 10:55:36 +0300

    Create Students.js

---
+++ Software Dev Concepts/Students.js
@@ -0,0 +1,8 @@
+{
+  "students" : [
+    {"name" : "Marry", "year" : 2019},
+    {"name" : "Steve" , "year" : 2020},
+    {"name" : "Nakov" , "year" : 2013},
+    {"name" : "Teddy" , "year" : 2020}
+  ]
+}
