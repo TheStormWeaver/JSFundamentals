d6dabc7c567dacb8546e43f9191b962c968d6e4a
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-10-23 15:12:12 +0300

    Update 4.Non-DecreasingSubsequence.js

--- Arrays More Exercises/4.Non-DecreasingSubsequence.js
+++ Arrays More Exercises/4.Non-DecreasingSubsequence.js
@@ -2,7 +2,7 @@ function nonDecreasing(arr) {
   let result = []
   let max = Number.MIN_SAFE_INTEGER
   for (let i = 0; i < arr.length; i++) {
-    if(arr[i] > max){
+    if(arr[i] >= max){
       max = arr[i]
       result.push(max)
     }
