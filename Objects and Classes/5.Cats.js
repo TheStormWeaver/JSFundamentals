900c3fec120843dbc0801682d0bb039dff791fae
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-10-25 14:13:29 +0200

    Create 5.Cats.js

---
+++ Objects and Classes/5.Cats.js
@@ -0,0 +1,18 @@
+function cats(catsAsStrings) {
+  class Cat{
+    constructor(name, age){
+      this.name = name
+      this.age = age
+    }
+
+    meow(){
+      console.log(`${this.name}, age ${this.age} says Meow`)
+    }
+  }
+  for (let catAsString of catsAsStrings) {
+    let [catName, catAge] = catAsString.split(" ")
+    let cat = new Cat(catName, catAge)
+    cat.meow()
+  }
+}
+cats(["Mellow 2","Tom 5"]);
