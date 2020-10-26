3576b81b0aae83d2abff2b84287ef60147be9173
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-10-26 14:01:35 +0200

    Update 4.Movies.js

--- Objects and Classes Exercise/4.Movies.js
+++ Objects and Classes Exercise/4.Movies.js
@@ -1,40 +1,38 @@
 function solve(arr) {
-  let movies = []
+  let movies = [];
   for (const line of arr) {
-    if(line.includes("addMovie")){
-      let currentMovie = {}
-      let name = line.substring(9)
-      currentMovie.name = name
-      movies.push(currentMovie)
-    }else if(line.includes("directedBy")){
-      let [name, director] = line.split(" directedBy ")
-      let myMovie = movies.find(x => x.name === name)
-      if(myMovie){
-        myMovie.director = director
+    if (line.includes("addMovie")) {
+      let currentMovie = {};
+      let name = line.substring(9);
+      currentMovie.name = name;
+      movies.push(currentMovie);
+    } else if (line.includes("directedBy")) {
+      let [movie, director] = line.split(" directedBy ");
+      let myMovie = movies.find((x) => x.name === movie);
+      if (myMovie) {
+        myMovie.director = director;
       }
-    }else if(line.includes("onDate")){
-      let [name, date] = line.split(" onDate ")
-      let myMovie = movies.find(x => x.name === name)
-      if(myMovie){
-        myMovie.date = date
+    } else if (line.includes("onDate")) {
+      let [movie, date] = line.split(" onDate ");
+      let myMovie = movies.find((x) => x.name === movie);
+      if (myMovie) {
+        myMovie.date = date;
       }
     }
   }
-  movies.forEach(movie => {
-    if(movie.name && movie.director && movie.date){
-      console.log(JSON.stringify(movie))
+  movies.forEach((movie) => {
+    if (movie.name && movie.director && movie.date) {
+      console.log(JSON.stringify(movie));
     }
-  })
+  });
 }
-solve(
-  [
-    'addMovie Fast and Furious',
-    'addMovie Godfather',
-    'Inception directedBy Christopher Nolan',
-    'Godfather directedBy Francis Ford Coppola',
-    'Godfather onDate 29.07.2018',
-    'Fast and Furious onDate 30.07.2018',
-    'Batman onDate 01.08.2018',
-    'Fast and Furious directedBy Rob Cohen'
-    ]    
-);
+solve([
+  "addMovie Fast and Furious",
+  "addMovie Godfather",
+  "Inception directedBy Christopher Nolan",
+  "Godfather directedBy Francis Ford Coppola",
+  "Godfather onDate 29.07.2018",
+  "Fast and Furious onDate 30.07.2018",
+  "Batman onDate 01.08.2018",
+  "Fast and Furious directedBy Rob Cohen",
+]);
