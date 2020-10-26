5e858378848ddade6781087064ea5c20189b00eb
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-10-26 14:50:26 +0200

    Create 5.Inventory.js

---
+++ Objects and Classes Exercise/5.Inventory.js
@@ -0,0 +1,29 @@
+function solve(arr) {
+  let heroes = [];
+  for (const line of arr) {
+    let [hero, level, items] = line.split(" / ");
+    items = items
+      .split(", ")
+      .sort((a, b) => a.localeCompare(b))
+      .join(", ");
+
+    let heroObject = {
+      Hero: hero,
+      level: Number(level),
+      items,
+    };
+    heroes.push(heroObject);
+  }
+  heroes
+    .sort((a, b) => a.level - b.level)
+    .forEach((hero) => {
+      console.log(`Hero: ${hero.Hero}`);
+      console.log(`level => ${hero.level}`);
+      console.log(`items => ${hero.items}`);
+    });
+}
+solve([
+  "Isacc / 25 / Apple, GravityGun",
+  "Derek / 12 / BarrelVest, DestructionSword",
+  "Hes / 1 / Desolator, Sentinel, Antara",
+]);
