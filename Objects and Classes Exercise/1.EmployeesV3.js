05e5da6c1a81f141ee40c8de4b9646385f45f95f
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-10-26 11:41:57 +0200

    Create 1.EmployeesV3.js

---
+++ Objects and Classes Exercise/1.EmployeesV3.js
@@ -0,0 +1,21 @@
+function employees(arr) {
+  class Person {
+    constructor(name) {
+      this.name = name;
+      this.number = name.length;
+    }
+    print() {
+      return `Name: ${this.name} -- Personal Number: ${this.number}`;
+    }
+  }
+  arr = arr.map((x) =>  new Person(x));
+  arr.forEach((obj) => {
+    console.log(obj.print());
+  });
+}
+employees([
+  "Silas Butler",
+  "Adnaan Buckley",
+  "Juan Peterson",
+  "Brendan Villarreal",
+]);
