4ba437aa23f7a8bb6fe25d508a68d6de7067e88f
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-10-30 15:18:22 +0200

    Update 6.SignCheck.js

--- Functions/6.SignCheck.js
+++ Functions/6.SignCheck.js
@@ -2,11 +2,11 @@ function solve(numOne, numTwo, numThree) {
   let result = "";
   if (numOne >= 0 && numTwo >= 0 && numThree >= 0) {
     result = "Positive";
-  } else if (numOne >= 0 && numTwo < 0 && numThree < 0) {
+  } else if (numOne >= 0 && numTwo <= 0 && numThree <= 0) {
     result = "Positive";
-  } else if (numOne < 0 && numTwo >= 0 && numThree < 0) {
+  }else if (numOne <= 0 && numTwo >= 0 && numThree <= 0) {
     result = "Positive";
-  } else if (numOne < 0 && numTwo < 0 && numThree >= 0) {
+  }else if (numOne <= 0 && numTwo <= 0 && numThree >= 0) {
     result = "Positive";
   }else{
     result = "Negative"
