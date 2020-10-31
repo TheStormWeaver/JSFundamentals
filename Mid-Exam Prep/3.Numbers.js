c916d8d76fa10bf176988271f36385757db17ef6
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-10-31 14:16:28 +0200

    Update 3.Numbers.js

--- Mid-Exam Prep/3.Numbers.js
+++ Mid-Exam Prep/3.Numbers.js
@@ -1,27 +1,34 @@
-function solve(input) {
-    let numbers = input.split(" ").map(Number);
-
-    let sum = 0;
-    numbers.forEach(x => sum += x);
-
-    let average = (sum / numbers.length).toFixed(2);
-
-    let greaterThanAvg = [];
-    numbers.forEach(x => {
-        if (x > average) {
-            greaterThanAvg.push(x);
-        }
-    });
-
-    if (greaterThanAvg.length < 1) {
-        console.log("No");
-    } else {
-        let topFive = [];
-        topFive = greaterThanAvg.sort((a, b) => b - a);
-        topFive.length = Math.min(topFive.length, 5);
-        console.log(topFive.join(" "));
+/*
+Write a program to read a sequence of integers and find and print the top 5 numbers that are greater than the average value in the sequence, sorted in descending order.
+Input
+Read from the console a single line holding space separated number.
+Output
+Print the above described numbers on a single line, space separated.
+If less than 5 numbers hold the above mentioned property, print less than 5 numbers. Print “No” if no numbers hold the above property.
+Constraints
+All input numbers are integers in range [-1 000 000 … 1 000 000]. The count of numbers is in range [1…10 000].
+*/
+function numbers(input) {
+  let result = []
+  let sum = 0
+  let arr = input.split(" ").map(Number)
+  for (const element of arr) {
+    sum += element
+  }
+  sum = sum / arr.length
+  for (const num of arr) {
+    if(num > sum){
+      result.push(num)
     }
-
+  }
+  if(result.length > 0){
+    result = result.sort((a, b) => b - a)
+    if(result.length > 5){
+      result.length = 5
+    }
+    console.log(result.join(" "))
+  }else{
+    console.log("No")
+  }
 }
-
-solve('20');
+numbers('5 2 3 4 -10 30 40 50 20 50 60 60 51');
