efe6c42b11eaf3b0d2375579c32fa44caab4e7ed
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-11-02 15:09:19 +0200

    Create 7.SearchforaNumber.js

---
+++ Arrays Advanced Exercises/7.SearchforaNumber.js
@@ -0,0 +1,24 @@
+*/
+function searchForNum(arr, commands) {
+  let newArr = [];
+  let elementsToTake = commands.shift();
+  let elementsToDelete = commands.shift();
+  let elementsToSearch = commands.shift();
+  newArr = arr.slice(0, elementsToTake)
+  newArr.length = elementsToTake;
+  for (let j = 0; j < elementsToTake; j++) {
+    newArr[j] = arr[j];
+  }
+  for (let j = 1; j <= elementsToDelete; j++) {
+    newArr.shift();
+  }
+  let occureCount = 0;
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i] === elementsToSearch) {
+      occureCount++;
+    }
+  }
+  console.log(`Number ${elementsToSearch} occurs ${occureCount} times.`);
+}
+searchForNum([5, 2, 3, 4, 1, 6], [5, 2, 3]);
+/*
