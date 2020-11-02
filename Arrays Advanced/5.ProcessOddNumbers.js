29fbe3b3cb30cb4cd66bf8df1f7c2c1eda21d99f
TheStormWeaver <70431835+TheStormWeaver@users.noreply.github.com>
2020-11-02 12:28:27 +0200

    Update 5.ProcessOddNumbers.js

--- Arrays Advanced/5.ProcessOddNumbers.js
+++ Arrays Advanced/5.ProcessOddNumbers.js
@@ -7,3 +7,12 @@ function Oddprocess(arr) {
   console.log(doubled.reverse())
 }
 Oddprocess([10, 15, 20, 25]);
+
+/*
+function Oddprocess(arr) {
+  let filtered = arr.filter((val, index) => index % 2 == 1)
+  let doubled = filtered.map(x => x * 2)
+  console.log(doubled.reverse())
+}
+Oddprocess([10, 15, 20, 25]);
+*/
