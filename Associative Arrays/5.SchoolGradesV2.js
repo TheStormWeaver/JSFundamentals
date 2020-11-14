e7fd8023dd88386aac4c13953105eca3d2bf8bba
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-11-14 17:50:35 +0200

    Update 5.SchoolGradesV2.js

--- Associative Arrays/5.SchoolGradesV2.js
+++ Associative Arrays/5.SchoolGradesV2.js
@@ -1,33 +1,33 @@
 function schoolGrades(input) {
-  let students = new Map()
+  let students = {};
   for (const line of input) {
-    let tokens = line.split(" ")
-    let name = tokens.shift()
-    let grades = tokens.map(Number)
-    if(students.has(name)){
-      let existing = students.get(name)
+    let tokens = line.split(" ");
+    let name = tokens.shift();
+    let grades = tokens.map(Number);
+    if (students.hasOwnProperty(name)) {
+      let existing = students[name];
       for (const grade of grades) {
-        existing.push(grade)
+        existing.push(grade);
       }
-    }else{
-      students.set(name, grades)
+    } else {
+      students[name] = grades;
     }
   }
-  let sorted = Array.from(students)
-  sorted.sort(compareAvg)
+  let sorted = Object.entries(students);
+  sorted.sort(compareAvg);
   for (const [name, grades] of sorted) {
-    console.log(`${name}: ${grades.join(", ")}`)
+    console.log(`${name}: ${grades.join(", ")}`);
   }
-  function compareAvg([nameA, gradesA],[nameB, gradesB]){
-    let avgA = 0
-    gradesA.forEach(x => avgA += x)
-    avgA /= gradesA.length
-    
-    let avgB = 0
-    gradesB.forEach(x => avgB += x)
-    avgB /= gradesB.length
+  function compareAvg([nameA, gradesA], [nameB, gradesB]) {
+    let avgA = 0;
+    gradesA.forEach((x) => (avgA += x));
+    avgA /= gradesA.length;
 
-    return avgA - avgB
+    let avgB = 0;
+    gradesB.forEach((x) => (avgB += x));
+    avgB /= gradesB.length;
+
+    return avgA - avgB;
   }
 }
 schoolGrades(["Lilly 4 6 6 5", "Tim 5 6", "Tammy 2 4 3", "Tim 6 6"]);
