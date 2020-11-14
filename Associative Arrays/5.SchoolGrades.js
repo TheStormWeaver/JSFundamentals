e17fc35f65201939e8daad3a0d61272cc2a28fc0
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-11-14 17:35:51 +0200

    Create 5.SchoolGrades.js

---
+++ Associative Arrays/5.SchoolGrades.js
@@ -0,0 +1,37 @@
+function schoolGrades(input) {
+  let students = new Map()
+  for (const line of input) {
+    let tokens = line.split(" ")
+    let name = tokens.shift()
+    let grades = tokens.map(Number)
+    if(students.has(name)){
+      let existing = students.get(name)
+      for (const grade of grades) {
+        existing.push(grade)
+      }
+    }else{
+      students.set(name, grades)
+    }
+  }
+  let sorted = Array.from(students)
+  sorted.sort(compareAvg)
+  for (const [name, grades] of sorted) {
+    console.log(`${name}: ${grades.join(", ")}`)
+  }
+  function compareAvg([nameA, gradesA],[nameB, gradesB]){
+    let avgA = 0
+    for (const element of gradesA) {
+      avgA += element
+    }
+    avgA /= gradesA.length
+    
+    let avgB = 0
+    for (const element of gradesB) {
+      avgB += element
+    }
+    avgB /= gradesB.length
+
+    return avgA - avgB
+  }
+}
+schoolGrades(["Lilly 4 6 6 5", "Tim 5 6", "Tammy 2 4 3", "Tim 6 6"]);
