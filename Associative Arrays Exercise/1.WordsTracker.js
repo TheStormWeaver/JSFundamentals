cda07667eee1affc14c3a074670c6d40b39e64f8
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-11-15 12:45:38 +0200

    Create 1.WordsTracker.js

---
+++ Associative Arrays Exercise/1.WordsTracker.js
@@ -0,0 +1,20 @@
+function wordsTracker(input) {
+  let words = input.shift().split(" ")
+  let wordsObj = {}
+  for (const word of words) {
+    wordsObj[word] = 0
+  }
+  for (const word of input){
+    if(wordsObj.hasOwnProperty(word)){
+      wordsObj[word] += 1
+    }
+  }
+  let keysWords = Object.keys(wordsObj).sort((a, b) => wordsObj[b] - wordsObj[a])
+  for (const element of keysWords) {
+    console.log(`${element} - ${wordsObj[element]}`)
+  }
+}
+wordsTracker([
+  'this sentence', 'In','this','sentence','you','have','to','count','the','occurances','of','the'
+  ,'words','this','and','sentence','because','this','is','your','task','sentence','sentence'
+  ]);
