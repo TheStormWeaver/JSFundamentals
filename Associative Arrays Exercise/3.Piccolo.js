889efbf5e73e713999385fffa7f46fe257593100
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-11-15 14:42:49 +0200

    Create 3.Piccolo.js

---
+++ Associative Arrays Exercise/3.Piccolo.js
@@ -0,0 +1,29 @@
+function piccolo(data) {
+  let parking = {}
+  for (const line of data) {
+    let [direction, carNumber] = line.split(", ")
+    if(direction == "IN"){
+      parking[carNumber] = 1
+    }else{
+      delete parking[carNumber]
+    }
+  }
+  let sorted = Object.keys(parking).sort((a,b) => a.localeCompare(b))
+  if(sorted.length > 0){
+  console.log(sorted.join(`\n`))
+  }else{
+    console.log(`Parking Lot is Empty`)
+  }
+}
+piccolo([
+  "IN, CA2844AA",
+  "IN, CA1234TA",
+  "OUT, CA2844AA",
+  "IN, CA9999TT",
+  "IN, CA2866HI",
+  "OUT, CA1234TA",
+  "IN, CA2844AA",
+  "OUT, CA2866HI",
+  "IN, CA9876HH",
+  "IN, CA2822UU",
+]);
