9a94154c66584a8213a805309375cdfcb43238b2
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-11-22 14:36:41 +0200

    Create 6.Pascal-CaseSplitter.js

---
+++ Text Processing - Exercise/6.Pascal-CaseSplitter.js
@@ -0,0 +1,14 @@
+function pascalCaseSplitter(str) {
+  let capitalIndex = 0
+  let result = []
+  for (let i = 1; i < str.length; i++) {
+    if(str[i].charCodeAt() >= 60 && str[i].charCodeAt() <= 90){
+      let word = str.substring(capitalIndex, i)
+      result.push(word)
+      capitalIndex = i
+    }
+  }
+  result.push(str.substring(capitalIndex, str.length))
+  console.log(result.join(", "))
+}
+pascalCaseSplitter('SplitMeIfYouCanHaHaYouCantOrYouCan');
