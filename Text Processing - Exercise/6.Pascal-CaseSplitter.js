f40eab16a7e336dd275006aa4ca4632739cc1400
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-11-22 14:38:54 +0200

    Update 6.Pascal-CaseSplitter.js

--- Text Processing - Exercise/6.Pascal-CaseSplitter.js
+++ Text Processing - Exercise/6.Pascal-CaseSplitter.js
@@ -2,7 +2,7 @@ function pascalCaseSplitter(str) {
   let capitalIndex = 0
   let result = []
   for (let i = 1; i < str.length; i++) {
-    if(str[i].charCodeAt() >= 60 && str[i].charCodeAt() <= 90){
+    if(str[i].charCodeAt() >= 65 && str[i].charCodeAt() <= 90){
       let word = str.substring(capitalIndex, i)
       result.push(word)
       capitalIndex = i
