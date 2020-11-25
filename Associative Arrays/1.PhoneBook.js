68f83574e53b1033de14bd764ec2b7aa054f5e9d
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-11-25 13:35:08 +0200

    Update 1.PhoneBook.js

--- Associative Arrays/1.PhoneBook.js
+++ Associative Arrays/1.PhoneBook.js
@@ -1,3 +1,8 @@
+/*
+Write a function that stores information about a person’s name and his phone number. The input comes as an array
+of strings. Each string contains the name and the number. If you receive the same name twice just replace the
+number. At the end print the result without sorting it. Try using an associative array.
+*/
 function phoneBook(input) {
   let phonebook = {}
   for (let line of input) {
