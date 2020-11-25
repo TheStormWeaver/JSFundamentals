5ac6721cbaf33d0b47527ebc95752a27d25cbd36
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-11-25 13:38:57 +0200

    Update 7.Neighborhoods.js

--- Associative Arrays/7.Neighborhoods.js
+++ Associative Arrays/7.Neighborhoods.js
@@ -1,3 +1,14 @@
+/*
+Write a function that receives list of neighborhoods and then some people, who are going to live in it. The input will
+come as array of strings. The first element will be the list of neighborhoods separated by &quot;, &quot;. The rest of the
+elements will be a neighborhood followed by a name of a person in the format "{neighborhood} - {person}".
+Add the person to the neighborhood only if the neighborhood is in the list of neighborhoods. At the end print the
+neighborhoods sorted by the count of inhabitants in descending order. Print them in the following format:
+"{neighborhood}: {inhabitants count}
+--{1 st inhabitant}
+--{2 nd inhabitant}
+…"
+*/
 function neighborhoods(arr) {
   let hood = {}
   let regions = arr.shift().split(", ");
