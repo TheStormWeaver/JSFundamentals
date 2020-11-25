078deeb50b6fc83b9bc98d8cd0a9d3e4df7acb98
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-11-25 13:35:35 +0200

    Update 2.Meetings.js

--- Associative Arrays/2.Meetings.js
+++ Associative Arrays/2.Meetings.js
@@ -1,3 +1,9 @@
+/*
+Write a function that manages meeting appointments. The input comes as an array of strings. Each string contains a
+weekday and person’s name. For each successful meeting, print a message. If you receive the same weekday twice,
+the meeting cannot be scheduled so print a conflict message. At the end print a list of all successful meetings. See
+example for message format and details.
+*/
 function Meetings(input) {
   let calendar = {}
   for (let line of input) {
