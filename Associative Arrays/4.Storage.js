fcf505b048755935f97d657334c196d8fa64c1ad
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-11-25 13:36:32 +0200

    Update 4.Storage.js

--- Associative Arrays/4.Storage.js
+++ Associative Arrays/4.Storage.js
@@ -1,3 +1,8 @@
+/*
+Write a function that takes a certain number of items and their quantity. If the same item appears more than once,
+add the new amount to the existing one. At the end print all the items and their amount without sorting them. The
+input comes as array of strings. Try using a Map().
+*/
 function storage(input) {
   let warehouse = new Map()
   for (const line of input) {
