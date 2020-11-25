4831d6804afa1c10c50b468ffa6d6b29f59bd387
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-11-25 13:35:59 +0200

    Update 3.AddressBook.js

--- Associative Arrays/3.AddressBook.js
+++ Associative Arrays/3.AddressBook.js
@@ -1,3 +1,8 @@
+/*
+Write a function that stores information about a person’s name and his address. The input comes as an array of
+strings. Each string contains the name and the address separated by a colon. If you receive the same name twice
+just replace the address. At the end print the full list, sorted alphabetically by the person’s name.
+*/
 function addressBook(input) {
   let catalog = {}
   for (const line of input) {
