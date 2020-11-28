0b6371d929bc66cfe2a42beabc237d033f07253b
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-11-28 20:27:14 +0200

    Update 1.MatchFullName.js

--- Regular Expressions/1.MatchFullName.js
+++ Regular Expressions/1.MatchFullName.js
@@ -1,10 +1,11 @@
 function matchName(input) {
-  let pattern = /\b[A-Z][a-z]+[ ][A-Z][a-z]+\b/g;
-  let validNames = [];
-  let validName = pattern.exec(input)
-  while (validName !== null) {
-    validNames.push(validName[0]);
-    validName = pattern.exec(input)
+    let pattern = /\b[A-Z][a-z]+[ ][A-Z][a-z]+\b/g;
+    let validNames = [];
+    let validName = pattern.exec(input)
+    while (validName !== null) {
+      validNames.push(validName[0]);
+      validName = pattern.exec(input)
+    }
+    console.log(validNames.join(" "));
   }
-  console.log(validNames.join(" "));
-}
+  matchName(["ivan ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Ivan IvAnov, Ivan Ivanov",]);
