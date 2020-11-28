87d5850fc41a5a931fe18e0f645183b27cee889e
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-11-28 20:45:29 +0200

    Create 2.MatchPhoneNumber.js

---
+++ Regular Expressions/2.MatchPhoneNumber.js
@@ -0,0 +1,11 @@
+function matchPhone([input]) {
+    let pattern = /\+359([ \-])2\1\d{3}\1\d{4}\b/g
+    let result = pattern.exec(input)
+    let validNums = []
+    while(result != null){
+        validNums.push(result[0])
+        result = pattern.exec(input)
+    }
+    console.log(validNums.join(", "))
+}
+matchPhone(["+359 2 222 2222,359-2-222-2222, +359/2/222/2222, +359-2 222 2222 +359 2-222-2222, +359-2-222-222, +359-2-222-22222 +359-2-222-2222",]);
