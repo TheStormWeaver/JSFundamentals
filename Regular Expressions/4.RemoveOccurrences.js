7bd4f030ae6a13809567a9cfeb14068cb69d099a
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-11-28 20:57:22 +0200

    Delete 4.RemoveOccurrences.js

--- Regular Expressions/4.RemoveOccurrences.js
+++
@@ -1,9 +0,0 @@
-function matchPhone(word, text) {
-  let old;
-  while (old !== text) {
-    old = text;
-    text = text.replace(word, "");
-  }
-  console.log(text);
-}
-matchPhone(["ice, kicegiciceeb"]);
