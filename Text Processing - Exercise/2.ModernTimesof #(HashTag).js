801503af1309ba34e77b59ff09d3f5bcf75de165
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-02 12:27:47 +0200

    Update 2.ModernTimesof #(HashTag).js

--- Text Processing - Exercise/2.ModernTimesof #(HashTag).js
+++ Text Processing - Exercise/2.ModernTimesof #(HashTag).js
@@ -1,24 +1,22 @@
 function timesofHashTag(text) {
-  text = text.split(" ")
-  let result = []
+  text = text.split(" ");
   for (let word of text) {
-    if(word.startsWith("#") && word.length > 1){
-      word = word.slice(1)
-      let isonlyletters = true
+    if (word.startsWith("#") && word.length > 1) {
+      word = word.slice(1);
+      let isonlyletters = true;
       for (const char of word) {
-        let symbol = char.charCodeAt()
-        if(symbol < 65 || symbol > 90 && symbol < 97 || symbol > 122){
+        let symbol = char.charCodeAt();
+        if (symbol < 65 || (symbol > 90 && symbol < 97) || symbol > 122) {
           isonlyletters = false;
           break;
         }
       }
-      if(isonlyletters){
-        
-        console.log(word)
+      if (isonlyletters) {
+        console.log(word);
       }
     }
   }
 }
 timesofHashTag(
-  'Nowadays everyone uses # to tag a #special word in #socialMedia'
+  "Nowadays everyone uses # to tag a #special word in #socialMedia"
 );
