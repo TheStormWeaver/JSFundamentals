974e1d4e66a3d51a42af01dfff3f8e90c583f816
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-02 12:14:50 +0200

    Update 2.Substring.js

--- Text Processing/2.Substring.js
+++ Text Processing/2.Substring.js
@@ -1,3 +1,7 @@
+/*
+Write a function that receives a string and two numbers. The numbers will be a starting index and count of
+elements to substring. Print the result.
+*/
 function substring(string, start, count) {
   let result = string.substring(start, start + count)
   console.log(result)
