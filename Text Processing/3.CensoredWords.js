9bab7a3ec7ea3760f963cb0520d32ea949af837c
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-02 12:16:27 +0200

    Update 3.CensoredWords.js

--- Text Processing/3.CensoredWords.js
+++ Text Processing/3.CensoredWords.js
@@ -1,6 +1,6 @@
 /*
 Write a function that receives a text as a first parameter and a single word as a second. Find all occurrences of that
-word in the text and replace them with the corresponding count of &#39;*&#39;.
+word in the text and replace them with the corresponding count of "*".
 */
 function censoredWords(text, word) {
   let result = text.split(word).join("*".repeat(word.length))
