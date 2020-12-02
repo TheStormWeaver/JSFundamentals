fd4f0e3fabe51d443f9f9a250115afa6d91e1dbd
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-02 12:16:10 +0200

    Update 4.CountStringOccurrencesV2.js

--- Text Processing/4.CountStringOccurrencesV2.js
+++ Text Processing/4.CountStringOccurrencesV2.js
@@ -1,3 +1,7 @@
+/*
+Write a function that receives a text and a string that you need to search. Print all the occurrences of that word in
+the string.
+*/
 function countStrOccurrs(text, word) {
   let result = text.split(" ").filter(c => c == word).length
   console.log(result)
