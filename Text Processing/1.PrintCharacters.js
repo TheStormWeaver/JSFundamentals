9830b4cad5dcc1a2947fcb612e3e8ff08b78a0ca
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-02 12:14:27 +0200

    Update 1.PrintCharacters.js

--- Text Processing/1.PrintCharacters.js
+++ Text Processing/1.PrintCharacters.js
@@ -1,3 +1,6 @@
+/*
+Write a function that receives a string and prints all the characters on separate lines.
+*/
 function printCharacters(input) {
   for (const char of input) {
     console.log(char)
