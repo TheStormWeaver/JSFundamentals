ca3a608fd3fe89bca5f38848d9eb58c5abcd7ba1
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-04 14:25:28 +0200

    Create 4.RemoveOccurrences.js

---
+++ Regular Expressions/4.RemoveOccurrences.js
@@ -0,0 +1,14 @@
+function matchPhone(word, text) {
+  let current = "";
+  let final = "";
+  while (text.includes(word)) {
+    current = text.indexOf(word);
+    final = current + word.length - 1;
+    text = text.split("");
+    for (let i = current; i <= final; i++) {
+      text.splice(current, 1);
+    }
+    text = text.join("");
+  }
+  console.log(text)
+}
