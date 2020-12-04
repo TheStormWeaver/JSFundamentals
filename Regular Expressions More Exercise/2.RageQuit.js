ec607c4534856a777a4d23d0c83b51c6f08ca101
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-04 17:17:12 +0200

    Update 2.RageQuit.js

--- Regular Expressions More Exercise/2.RageQuit.js
+++ Regular Expressions More Exercise/2.RageQuit.js
@@ -1,11 +1,11 @@
 function rageQuit(input) {
-  let string = input[0];
+  let first = input[0];
   let uniqueChars = [];
-  let series = string.match(/[^\d]+\d+/g);
+  let elements = first.match(/[^\d]+\d+/g);
   let print = "";
-  for (let elem of series) {
-    let num = Number(/\d+/g.exec(elem)[0]);
-    let chars = /[^\d]+/g.exec(elem)[0];
+  for (let element of elements) {
+    let num = Number(/\d+/g.exec(element)[0]);
+    let chars = /[^\d]+/g.exec(element)[0];
     chars = chars.toUpperCase();
     if (num !== 0) {
       for (let char of chars) {
