2b03df5aae8ede5aa33cb64dbd651516b38655ae
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-05 13:43:57 +0200

    Create 1.ActivationKeysV2.js

---
+++ Final Exam - Prep/Final Exam - 04 April 2020 Group 1/1.ActivationKeysV2.js
@@ -0,0 +1,58 @@
+function activationKeys(input) {
+  let key = input.shift();
+  let line;
+  while ((line = input.shift()) != "Generate") {
+    let [action, ...params] = line.split(">>>");
+    switch (action) {
+      case "Contains":
+        key = Contains(key, ...params);
+        break;
+      case "Flip":
+        key = Flip(key, ...params);
+        break;
+      case "Slice":
+        key = Slice(key, ...params);
+    }
+  }
+  console.log(`Your activation key is: ${key}`)
+  function Contains(key, pattern) {
+    if (key.includes(pattern)) {
+      console.log(`${key} contains ${pattern}`);
+    } else {
+      console.log(`Substring not found!`);
+    }
+    return key;
+  }
+  function Flip(key, mode, start, end) {
+    [start, end] = [Number(start), Number(end)];
+    let first = key.substring(0, start);
+    let second = key.substring(start, end);
+    let third = key.substring(end);
+    if (mode == "Upper") {
+      second = second.toUpperCase();
+    } else if (mode == "Lowercase") {
+      second = second.toLowerCase();
+    }
+    let result = first + second + third;
+    console.log(result);
+    return result;
+  }
+  function Slice(key, start, end) {
+    [start, end] = [Number(start), Number(end)];
+    let first = key.substring(0, start);
+    let second = key.substring(start, end);
+    let third = key.substring(end);
+    let result = first + third;
+    console.log(result);
+    return result;
+  }
+}
+activationKeys([
+  "abcdefghijklmnopqrstuvwxyz",
+  "Slice>>>2>>>6",
+  "Flip>>>Upper>>>3>>>14",
+  "Flip>>>Lower>>>5>>>7",
+  "Contains>>>def",
+  "Contains>>>deF",
+  "Generate",
+]);
