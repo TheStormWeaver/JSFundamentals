f8053fc98f8ff06fa91da33a4ae570f17ddbdfcb
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-24 16:12:32 +0200

    Update 6.Smallest2Numbers.js

--- Arrays Advanced/6.Smallest2Numbers.js
+++ Arrays Advanced/6.Smallest2Numbers.js
@@ -1,3 +1,8 @@
+/*
+Write a function that prints the two smallest elements from an array of numbers.
+The input comes as array of number elements.
+The output is printed on the console on a single line, separated by space.
+*/
 function smallestTwoNumbers(arr) {
   arr.sort((a, b) => a - b);
   let result = arr.slice(0, 2);
