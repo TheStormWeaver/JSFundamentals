5c78237f070a24ef0dbb4ff841a89d1c9e13774c
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-24 16:11:55 +0200

    Update 4.LastKNumbersSequence.js

--- Arrays Advanced/4.LastKNumbersSequence.js
+++ Arrays Advanced/4.LastKNumbersSequence.js
@@ -1,3 +1,12 @@
+/*
+You are given two integers n and k. Write a function that generates and prints the following sequence:
+ The first element is 1.
+ Every following element equals the sum of the previous k elements.
+ The length of the sequence is n elements.
+The input comes as two number arguments. The first element represents the number n, and the second – the
+number k.
+The output is printed on the console on a single line, separated by space.
+*/
 function sumofK(n, k) {
   let result = [1]
   for (let i = 1; i < n; i++) {
