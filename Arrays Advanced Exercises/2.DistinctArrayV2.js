d4b1e0243bdf2faee76eb83eea39c4f42b67d53e
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-24 16:05:53 +0200

    Update 2.DistinctArrayV2.js

--- Arrays Advanced Exercises/2.DistinctArrayV2.js
+++ Arrays Advanced Exercises/2.DistinctArrayV2.js
@@ -1,3 +1,8 @@
+/*
+You will be given an array of integer numbers on the first line of the input (space-separated). 
+Remove all repeating elements from the array. 
+Print the result elements separated by single space.
+*/
 function Distinct(arr) {
   for (let i = 0; i < arr.length; i++) {
     let index = arr.indexOf(arr[i], i + 1);
