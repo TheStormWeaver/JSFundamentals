cb8cc89fb1e3c5c6c0082043265e5a3f50d43674
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-24 16:11:02 +0200

    Update 2.NegativePositiveNumbers.js

--- Arrays Advanced/2.NegativePositiveNumbers.js
+++ Arrays Advanced/2.NegativePositiveNumbers.js
@@ -1,3 +1,9 @@
+/*
+Write a function that processes the elements in an array one by one and produces a new array. Prepend each
+negative element at the front of the result and append each positive (or 0) element at the end of the result.
+The input comes as array of number elements.
+The output is printed on the console, each element on a new line.
+*/
 function process(arr) {
   let result = []
   for (let num of arr) {
