8b2ef856574390fb9d71b55bf571ee9413a6d839
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-24 16:12:16 +0200

    Update 5.ProcessOddNumbers.js

--- Arrays Advanced/5.ProcessOddNumbers.js
+++ Arrays Advanced/5.ProcessOddNumbers.js
@@ -1,3 +1,9 @@
+/*
+You are given an array of numbers. Write a function that prints the elements at odd positions from the array,
+doubled and in reverse order.
+The input comes as array of number elements.
+The output is printed on the console on a single line, separated by space.
+*/
 function Oddprocess(arr) {
   let filtered = arr.filter(isOdd)
   let doubled = filtered.map(x => x * 2)
