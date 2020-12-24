62c7ca6665a38d557faa521a43bee4c823f1f171
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-24 16:13:12 +0200

    Update 8.ArrayManipulations.js

--- Arrays Advanced/8.ArrayManipulations.js
+++ Arrays Advanced/8.ArrayManipulations.js
@@ -1,3 +1,15 @@
+/*
+Write a function that manipulates an array of numbers.
+ Add {number}: add a number to the end of the array
+ Remove {number}: remove number from the array
+ RemoveAt {index}: removes number at a given index
+ Insert {number} {index}: inserts a number at a given index
+Note: All the indices will be valid!
+Print the final state of the array (separated by single space).
+The input comes as array of strings. First element will be a string containing the array to manipulate. Every other
+command you receive will also be a string.
+The output is the manipulated array printed on the console on a single line, separated by space.
+*/
 function arrayManipulations(input) {
   let arr = input.shift().split(" ").map(Number);
   for (let line of input) {
