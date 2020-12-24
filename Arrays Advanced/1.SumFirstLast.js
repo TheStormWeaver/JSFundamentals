51dff49ce4bdf2fdf2696b81c28d6e9112c6869b
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-24 16:10:26 +0200

    Update 1.SumFirstLast.js

--- Arrays Advanced/1.SumFirstLast.js
+++ Arrays Advanced/1.SumFirstLast.js
@@ -1,3 +1,8 @@
+/*
+Write a function that calculates and prints the sum of the first and the last elements in an array.
+The input comes as array of string elements holding numbers.
+The output is the return value of your function.
+*/
 function sum(arr) {
   let finalsum = Number(arr[0]) + Number(arr[arr.length - 1]);
   console.log(finalsum);
