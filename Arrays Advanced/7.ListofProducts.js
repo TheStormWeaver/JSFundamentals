f23058250238f82e2688aaa7e701b55c60b03916
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-24 16:12:48 +0200

    Update 7.ListofProducts.js

--- Arrays Advanced/7.ListofProducts.js
+++ Arrays Advanced/7.ListofProducts.js
@@ -1,3 +1,6 @@
+/*
+You will receive an array of products. Print a numbered array of all the products ordered by name.
+*/
 function listOfProducts(input) {
   let sorted = input.sort((a, b) => a.localeCompare(b))
   for (let i = 0; i < input.length; i++) {  
