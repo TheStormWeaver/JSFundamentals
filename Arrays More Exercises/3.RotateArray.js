22946edd3297fc0dcd5b1bad43b7f997fb87800e
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-26 10:56:08 +0200

    Update 3.RotateArray.js

--- Arrays More Exercises/3.RotateArray.js
+++ Arrays More Exercises/3.RotateArray.js
@@ -1,3 +1,10 @@
+/*
+Write a function that rotates an array. The array should be rotated to the right side, meaning that the last element
+should become the first, upon rotation.
+The input comes as array of strings. The last element of the array is the amount of rotation you need to perform.
+The output is the resulted array after the rotations. The elements should be printed on one line, separated by a
+single space.
+*/
 function arrRotation(arr) {
   let rotations = Number(arr.pop())
   for (let i = 0; i < rotations; i++) {
