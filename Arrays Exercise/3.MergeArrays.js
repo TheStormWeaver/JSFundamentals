4ac276fa7e31b5a381bd722a1da46d74a66ab646
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-26 10:53:12 +0200

    Update 3.MergeArrays.js

--- Arrays Exercise/3.MergeArrays.js
+++ Arrays Exercise/3.MergeArrays.js
@@ -2,6 +2,8 @@
 Write a function which receives two string arrays and merges them into a third array.
 If the index of the element is even, add into the third array the sum of both elements at that index
 If the index of the element is odd, add the concatenation of both elements at that index
+Output
+As output you should print the resulting third array, each element separated by " - ".
 */
 function solve(arr1, arr2) {
   let finalarr = []
