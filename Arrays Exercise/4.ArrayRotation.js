3ac9e46344060aa9ca198b8fb71fd8f1c5678901
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-26 10:53:31 +0200

    Update 4.ArrayRotation.js

--- Arrays Exercise/4.ArrayRotation.js
+++ Arrays Exercise/4.ArrayRotation.js
@@ -1,3 +1,9 @@
+/*
+Write a function that receives an array and number of rotations you have to perform (first element goes at the
+end).
+Output
+Print the resulting array elements separated my single space.
+*/
 function solve(arr, rotations) {
   for (let i = 1; i <= rotations; i++) {
     let element = arr.shift()
