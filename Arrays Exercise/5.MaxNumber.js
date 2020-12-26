b1a37ad88c9bb827519974d874a8ac998ace84b1
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-26 10:53:49 +0200

    Update 5.MaxNumber.js

--- Arrays Exercise/5.MaxNumber.js
+++ Arrays Exercise/5.MaxNumber.js
@@ -1,3 +1,9 @@
+/*
+Write a function to find all the top integers in an array. A top integer is an integer which is bigger than all the
+elements to its right.
+Output
+Print all top integers on the console, separated by single space.
+*/
 function solve(arr) {
   let result = []
   for (let i = 0; i < arr.length; i++) {
