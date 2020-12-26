b9b6f8fa40016364eced3461b109bbcc4a37b8c8
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-26 10:56:31 +0200

    Update 4.Non-DecreasingSubsequence.js

--- Arrays More Exercises/4.Non-DecreasingSubsequence.js
+++ Arrays More Exercises/4.Non-DecreasingSubsequence.js
@@ -1,3 +1,14 @@
+/*
+Write a function that extracts only those numbers that form a non-decreasing subsequence. In other words, you
+start from the first element and continue to the end of the given array of numbers. Any number which is LESS THAN
+the current biggest one is ignored, alternatively if it’s equal or higher than the current biggest one you set it as the
+current biggest one and you continue to the next number.
+Input
+The input comes as array of numbers.
+Output
+The output is the processed array after the filtration, which should be a non-decreasing subsequence. The elements
+should be printed on one line, separated by a single space.
+*/
 function nonDecreasing(arr) {
   let result = []
   let max = Number.MIN_SAFE_INTEGER
