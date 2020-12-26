b24194784f6e1a2405e9735fa169f6d1e78e854f
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-26 10:55:46 +0200

    Update 2.AddandRemove.js

--- Arrays More Exercises/2.AddandRemove.js
+++ Arrays More Exercises/2.AddandRemove.js
@@ -1,3 +1,15 @@
+/*
+Write a function that adds and removes numbers to/from an array. You will receive a command which can either be
+&quot;add&quot; or &quot;remove&quot;.
+The initial number is 1. Each input command should increase that number, regardless of what it is.
+Upon receiving an &quot;add&quot; command you should add the current number to your array.
+Upon receiving the &quot;remove&quot; command you should remove the last entered number, currently existent in
+the array.
+Input
+The input comes as array of strings. Each element holds a command.
+Output
+Print elements in a row, separated by single space. In case of an empty array, just print &quot;Empty&quot;.
+*/
 function addAndRemove(arr) {
   let result = [];
   let num = 0;
