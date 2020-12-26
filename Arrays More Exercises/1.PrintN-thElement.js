fc31aa2b440ac2a441c8b9dbd07f90d257646453
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-26 10:55:24 +0200

    Update 1.PrintN-thElement.js

--- Arrays More Exercises/1.PrintN-thElement.js
+++ Arrays More Exercises/1.PrintN-thElement.js
@@ -1,3 +1,10 @@
+/*
+Write a function that collect each element of an array, on a given step.
+The input comes as array of strings. The last element is N - the step.
+The collections are every element on the N-th step starting from the first one. If the step is &quot;3&quot;, you need to print
+the 1-st, the 4-th, the 7-th … and so on, until you reach the end of the array. Then, print elements in a row,
+separated by single space.
+*/
 function printElement(arr) {
   let n = Number(arr.pop());
   let print = "";
