0afa10d3aaaae4f107399ce2b618fc32d2386389
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-27 13:02:17 +0200

    Update 8.AMinerTask.js

--- Associative Arrays Exercise/8.AMinerTask.js
+++ Associative Arrays Exercise/8.AMinerTask.js
@@ -1,3 +1,9 @@
+/*
+You are given an array of strings. Every odd string is representing a resource (e.g. Gold, Silver, Copper, and so on), and every even – quantity. Your task is to collect the resources and print them each on a new line. 
+Print the resources and their quantities in format:
+{resource} –> {quantity}
+The quantities inputs will be in the range [1 … 2 000 000 000]
+*/
 function minerTask(arr) {
   let mine = {}
   for (let i = 0; i < arr.length; i += 2) {
