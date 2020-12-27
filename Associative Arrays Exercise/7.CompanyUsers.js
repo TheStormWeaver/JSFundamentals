f372378079929ed40700cf1e4b6d89d88b358cf1
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-27 13:01:36 +0200

    Update 7.CompanyUsers.js

--- Associative Arrays Exercise/7.CompanyUsers.js
+++ Associative Arrays Exercise/7.CompanyUsers.js
@@ -1,3 +1,13 @@
+/*
+Write a function which keeps information about companies and their employees. 
+You will receive array of strings containing company name and employee's id. Add each employee to the given company. Keep in mind that a company cannot have two employees with the same id.
+When you finish reading data, order the companies by the name in ascending order. 
+Print the company name and each employee's id in the following format:
+{companyName}
+-- {id1}
+-- {id2}
+-- {idN}
+*/
 function companyUsers(arr) {
   let companylist = {}
   for (const line of arr) {
