dbc653aaf9c56badd2affcc4c5891f50c522b927
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-27 13:00:03 +0200

    Update 3.Piccolo.js

--- Associative Arrays Exercise/3.Piccolo.js
+++ Associative Arrays Exercise/3.Piccolo.js
@@ -1,3 +1,11 @@
+/*
+Write function that:
+•	Records a car number for every car that enters the  parking lot
+•	Removes a car number when the car goes out
+•	Input will be array of strings in format [direction, carNumber]
+Print the output with all car numbers which are in the parking lot sorted in ascending by number
+
+*/
 function piccolo(data) {
   let parking = {}
   for (const line of data) {
