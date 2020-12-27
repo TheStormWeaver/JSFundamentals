a1b0ceb3c857c29b38a587f52ed275f9c87901fd
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-27 12:54:32 +0200

    Update 3.ReverseanArrayofNumbers.js

--- Arrays/3.ReverseanArrayofNumbers.js
+++ Arrays/3.ReverseanArrayofNumbers.js
@@ -1,3 +1,7 @@
+/*
+Write a program which receives a number n and an array of elements. Your task is to create a new array with n
+numbers, reverse it and print its elements on a single line, space separated.
+*/
 function solve(num, arr) {
   let newarr = []
   let result = ""
