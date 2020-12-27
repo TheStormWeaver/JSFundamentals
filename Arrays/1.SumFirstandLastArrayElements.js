e02b92da4e39581083828ce85e26c49c1cea54b1
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-27 12:53:35 +0200

    Update 1.SumFirstandLastArrayElements.js

--- Arrays/1.SumFirstandLastArrayElements.js
+++ Arrays/1.SumFirstandLastArrayElements.js
@@ -1,3 +1,6 @@
+/*
+Write a function that receives an array of strings and prints the sum of first and last element in that array.
+*/
 function solve(arr) {
   let sum = arr[0] + arr[arr.length-1]
   console.log(sum)
