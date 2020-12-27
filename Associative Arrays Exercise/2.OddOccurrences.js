34c71a8537128aa0f4b036c2fcc5899ef0ec85f7
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-27 12:58:40 +0200

    Update 2.OddOccurrences.js

--- Associative Arrays Exercise/2.OddOccurrences.js
+++ Associative Arrays Exercise/2.OddOccurrences.js
@@ -1,3 +1,7 @@
+/*
+Write a function that extracts all the elements of a sentence odd number of times (case-insensitive)
+The input comes as a single string. The words will be separated by a single space.
+*/
 function oddOccurrences(input) {
   let arr = input.split(" ")
   let occurrences = new Map()
