e92207a9270d0d7ad7f35ee6d32b6c32540aa54b
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-27 12:54:11 +0200

    Update 2.DayofWeek.js

--- Arrays/2.DayofWeek.js
+++ Arrays/2.DayofWeek.js
@@ -1,3 +1,7 @@
+/*
+Write a program which receives a number and prints the corresponding name of the day of week.
+If the number is NOT a valid day, print "Invalid day!".
+*/
 function solve(day) {
   let days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
   if(day >= 1 && day <= 7){
