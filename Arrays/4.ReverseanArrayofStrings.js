68fa12e1bfcb1298250f55c23e172561f87899bf
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-27 12:54:51 +0200

    Update 4.ReverseanArrayofStrings.js

--- Arrays/4.ReverseanArrayofStrings.js
+++ Arrays/4.ReverseanArrayofStrings.js
@@ -1,3 +1,7 @@
+/*
+Write a program which receives an array of strings (space separated values). Your task is to reverse it and print its
+elements. Swap elements.
+*/
 function reverse(arr) {
   let result = []
   for (let i = arr.length - 1; i >= 0; i--) {
