08a1d0f2fbe7372860e64af141c5c0ad784ca8f5
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-27 13:00:21 +0200

    Update 4.PartyTime.js

--- Associative Arrays Exercise/4.PartyTime.js
+++ Associative Arrays Exercise/4.PartyTime.js
@@ -1,3 +1,10 @@
+/*
+There is a party at SoftUni. Many guests are invited and they are two types: VIP and regular. When guests come to the party check if he/she exists in any of the two reservation lists. 
+The input will come as array of strings. You will be given the list with the guests before you receive a command "PARTY"
+All VIP numbers start with digit
+When you receive the command "PARTY" the guests start coming.
+Output all guest, who didn't come to the party (VIP must be first) 
+*/
 function partyTime(data) {
   let list = {
     "VIP": [],
