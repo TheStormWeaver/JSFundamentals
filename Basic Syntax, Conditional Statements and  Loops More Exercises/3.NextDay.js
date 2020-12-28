c2da030c457b81b7e2c710ed5421581820fa6768
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-28 11:03:43 +0200

    Update 3.NextDay.js

--- Basic Syntax, Conditional Statements and  Loops More Exercises/3.NextDay.js
+++ Basic Syntax, Conditional Statements and  Loops More Exercises/3.NextDay.js
@@ -1,3 +1,9 @@
+/*
+Write a JS function that calculates the date of the next day by given year, month and day.
+The input comes as three number parameters. The first element is the year, the second is the month and the third is
+the day.
+The output should be returned as a result of your function.
+*/
 function sortNumbers(year, month, day) {
     let date=new Date(year, month, day);
     let tomorrow=new Date(year, month-1, date.getDate()+1);
