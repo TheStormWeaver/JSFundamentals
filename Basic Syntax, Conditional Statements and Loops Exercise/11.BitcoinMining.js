cba22221b3bebd0a7e1e2d214ff6da5a0f8ebd41
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-28 11:11:08 +0200

    Update 11.BitcoinMining.js

--- Basic Syntax, Conditional Statements and Loops Exercise/11.BitcoinMining.js
+++ Basic Syntax, Conditional Statements and Loops Exercise/11.BitcoinMining.js
@@ -1,3 +1,26 @@
+/*
+Write a JavaScript program that calculates the total amount of bitcoins you purchased with the gold you mined
+during your shift at the mine. Your shift consists of a certain number of days where you mine an amount of gold in
+grams. Your program will receive an array with the amount of gold you mined each day, where the first day of your
+shift is the first index of the array. Also, someone was stealing every third day from the start of your shift 30% from
+the mined gold for this day. For the different exchanges use these prices:
+Input
+You will receive an array of numbers, representing your shift at the mine.
+Output
+Print on the console these lines in the following formats:
+First line prints the total amount of bought bitcoins:
+"Bought bitcoins: {count}"
+Second line prints which day you bought your first bitcoin:
+"Day of the first purchased bitcoin: {day}"
+In case you did not purchased any bitcoins, do not print the second line.
+Third line prints the amount of money that’s left after the bitcoin purchases rounded by the second digit
+after the decimal point:
+"Left money: {money} lv."
+Constraints
+The input array may contain up to 1,000 elements
+The numbers in the array are in range [0.01..5,000.00] inclusive
+Allowed time/memory: 100ms/16MB
+*/
 function solve(input) {
     let daylyEarnedGold = 0;
     let totalMinedGold = 0;
