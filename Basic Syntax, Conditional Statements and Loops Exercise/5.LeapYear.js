033f9eff798ba9bcfff9e2e036da219270607c17
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-28 11:06:58 +0200

    Update 5.LeapYear.js

--- Basic Syntax, Conditional Statements and Loops Exercise/5.LeapYear.js
+++ Basic Syntax, Conditional Statements and Loops Exercise/5.LeapYear.js
@@ -1,3 +1,7 @@
+/*
+Write a JS function to check whether a year is leap. Leap years are either divisible by 4 but not by 100 or are divisible
+by 400. Return the result like examples below:
+*/
 function solve(num) {
   if(num % 4 == 0 & num % 100 !== 0 || num % 400 == 0){
     console.log("yes")
