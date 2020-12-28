1e39e6ce27911ae53eb0fc7fc4b7f52c384c90a5
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-28 11:03:25 +0200

    Update 2.EnglishNameoftheLastDigit.js

--- Basic Syntax, Conditional Statements and  Loops More Exercises/2.EnglishNameoftheLastDigit.js
+++ Basic Syntax, Conditional Statements and  Loops More Exercises/2.EnglishNameoftheLastDigit.js
@@ -1,3 +1,7 @@
+/*
+Write a function that returns the English name of the last digit of a given number. Write a program that receives a
+number and prints the returned value from this function.
+*/
 function sortNumbers(arr) {
   arr = arr.toString();
   let split = arr.split("");
