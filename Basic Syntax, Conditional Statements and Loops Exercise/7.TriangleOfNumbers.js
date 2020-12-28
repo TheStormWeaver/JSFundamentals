dc8cf144da350e7592c3d9391913ef8992964e64
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-28 11:07:51 +0200

    Update 7.TriangleOfNumbers.js

--- Basic Syntax, Conditional Statements and Loops Exercise/7.TriangleOfNumbers.js
+++ Basic Syntax, Conditional Statements and Loops Exercise/7.TriangleOfNumbers.js
@@ -1,3 +1,8 @@
+/*
+Write a function, which receives a single number – n, and prints a triangle from 1 to n as in the examples.
+Constraints
+n will be in the interval [1...20].
+*/
 function solve(num) {
   for (let i = 1; i <= num; i++) {
     let line = "";
