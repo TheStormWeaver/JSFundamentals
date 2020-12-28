82090e78e58bbe5c69d0da73ea8b90fdcc0d5903
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-28 11:08:06 +0200

    Update 8.MultiplicationTable.js

--- Basic Syntax, Conditional Statements and Loops Exercise/8.MultiplicationTable.js
+++ Basic Syntax, Conditional Statements and Loops Exercise/8.MultiplicationTable.js
@@ -1,3 +1,12 @@
+/*
+You will receive a number as an input from the console. Print the 10 times table for this number. See the examples
+below for more information.
+Output
+Print every row of the table in the following format:
+{number} X {times} = {product}
+Constraints
+The number will be an integer will be in the interval [1…100]
+*/
 function solve(num) {
   for (i = 1; i <= 10; i++) {
     console.log(`${num} X ${i} = ${num * i}`)
