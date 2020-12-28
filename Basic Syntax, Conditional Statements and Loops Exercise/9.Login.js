e988b93c76417dd985c91236db6b5afcf93f1c63
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-28 11:09:14 +0200

    Update 9.Login.js

--- Basic Syntax, Conditional Statements and Loops Exercise/9.Login.js
+++ Basic Syntax, Conditional Statements and Loops Exercise/9.Login.js
@@ -1,3 +1,9 @@
+/*
+You will be given a string representing a username. The password will be that username reversed. Until you receive
+the correct password print on the console "Incorrect password. Try again.". When you receive the correct password
+print "User {username} logged in."; However on the fourth try if the password is still not correct print "User
+{username} blocked!" and end the program. The input comes as an array of strings.
+*/
 function solve(arr) {
   let username = arr.shift();
   let pass = username.split("").reverse().join("");
