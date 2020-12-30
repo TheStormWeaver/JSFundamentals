8692266224e05441b082163df1e41b67f141b765
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-30 14:56:47 +0200

    Update 11.SumofOddNumbers.js

--- Basic Syntax, Conditional Statements and Loops/11.SumofOddNumbers.js
+++ Basic Syntax, Conditional Statements and Loops/11.SumofOddNumbers.js
@@ -1,3 +1,13 @@
+/*
+Write a program that prints the next n odd numbers (starting from 1) and on the last row prints the sum of them.
+Input
+You will receive a number – n. This number shows how many odd numbers you should print.
+Output
+Print the next n odd numbers, starting from 1, separated by new lines. On the last line, print the sum of these
+numbers.
+Constraints
+n will be in the interval [1…100]
+*/
 function solve(n) {
   let sum = 0;
   for (let current = 1, count = 0; count < n; current += 2, count++) {
