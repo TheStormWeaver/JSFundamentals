0746d581b5ab6bb8d5df9a5295e21a2eb0d15a45
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-30 14:46:45 +0200

    Update 5.NumbersfromMtoN.js

--- Basic Syntax, Conditional Statements and Loops/5.NumbersfromMtoN.js
+++ Basic Syntax, Conditional Statements and Loops/5.NumbersfromMtoN.js
@@ -1,3 +1,7 @@
+/*
+Write a function that receives a number M and a number N (M will always be bigger than N). Print all numbers from
+M to N
+*/
 function solve(m, n) {
  for (i= m; i >= n; i--){
    console.log(i)
