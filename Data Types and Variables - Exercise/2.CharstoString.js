ed858fc0ae68f78dec7bf061c7255990094156da
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-30 14:57:39 +0200

    Update 2.CharstoString.js

--- Data Types and Variables - Exercise/2.CharstoString.js
+++ Data Types and Variables - Exercise/2.CharstoString.js
@@ -1,3 +1,7 @@
+/*
+Write a function which receives 3 parameters. Each parameter is a single character. Combine all the characters
+into one string and print it on the console.
+*/
 function solve(par1, par2, par3) {
   let combination = par1 + par2 + par3
   console.log(combination)
