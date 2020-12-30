24baf42843aa2c482802c43410383e032f3d0436
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-30 14:59:12 +0200

    Update 6.ReversedChars.js

--- Data Types and Variables - Exercise/6.ReversedChars.js
+++ Data Types and Variables - Exercise/6.ReversedChars.js
@@ -1,6 +1,5 @@
 /*
-Write a program that takes 3 parameters (characters) and prints them in reversed order with a space between
-them.
+Write a program that takes 3 parameters (characters) and prints them in reversed order with a space between them.
 */
 function solve(char1,char2,char3) {
   let reversedorder = char3 + " " + char2 + " " + char1 + " "
