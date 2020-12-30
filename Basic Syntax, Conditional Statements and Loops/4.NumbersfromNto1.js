97ff710c85fa35ca23fe0d31281eb11266e6881b
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-30 14:46:24 +0200

    Update 4.NumbersfromNto1.js

--- Basic Syntax, Conditional Statements and Loops/4.NumbersfromNto1.js
+++ Basic Syntax, Conditional Statements and Loops/4.NumbersfromNto1.js
@@ -1,3 +1,6 @@
+/*
+Create a function that receives a number N and prints all the numbers from N to 1. Try using while loop
+*/
 function solve(repeat) {
   while(1 <= repeat){
     console.log(repeat)
