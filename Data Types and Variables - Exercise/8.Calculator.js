e949aaf367f042578b79a29ddf4b4d1da3a22bdf
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-30 14:59:53 +0200

    Update and rename 8.Calculator to 8.Calculator.js

--- Data Types and Variables - Exercise/8.Calculator.js
+++ Data Types and Variables - Exercise/8.Calculator.js
@@ -1,3 +1,7 @@
+/*
+Write a function that receives 3 parameters: a number, an operator (string) and another number. Print the
+result of the calculation on the console formatted to the second decimal point
+*/
 function solve(num1,operator,num2) {
   let sum = 0
   if(operator == "+"){
