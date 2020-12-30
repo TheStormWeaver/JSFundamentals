d620c8e2c89fb37d629d0d2f75edfaf7f851cc37
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-30 14:45:44 +0200

    Update 2.Excellentgrade.js

--- Basic Syntax, Conditional Statements and Loops/2.Excellentgrade.js
+++ Basic Syntax, Conditional Statements and Loops/2.Excellentgrade.js
@@ -1,3 +1,7 @@
+/*
+Write a function that receives a single number and checks if the grade is excellent or not.
+If it is, print "Excellent", otherwise print "Not excellent"
+*/
 function solve(num) {
 if (num >= 5.50 & num <= 6){
 console.log("Excellent")
