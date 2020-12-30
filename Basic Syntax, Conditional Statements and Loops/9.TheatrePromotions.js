867639298875b3b43f39801388afd5d205ca5b80
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-30 14:55:52 +0200

    Update 9.TheatrePromotions.js

--- Basic Syntax, Conditional Statements and Loops/9.TheatrePromotions.js
+++ Basic Syntax, Conditional Statements and Loops/9.TheatrePromotions.js
@@ -1,3 +1,21 @@
+/*
+A theatre is doing a ticket sale, but they need a program to calculate the price of a single ticket. If the given age
+does not fit one of the categories, you should print &quot;Error!&quot;. You can see the prices in the table below:
+0 <= age <= 18  
+Weekday 12$  Weekend 15$ Holiday 5$
+18 <= age <= 64
+Weekday 18$ Weekend 20$ Holiday 12$
+64 <= age <= 122
+Weekday 12$ Weekend 15$ Holiday 10$ 
+Input
+The input comes in two parameters. The first one will be the type of day (string). The second – the age of the
+person (number).
+Output
+Print the price of the ticket according to the table, or "Error!" if the age is not in the table.
+Constraints
+The age will be in the interval [-1000…1000].
+The type of day will always be valid.
+*/
 function solve(day, age) {
   price = 0;
   if (0 <= age & age < 18) {
