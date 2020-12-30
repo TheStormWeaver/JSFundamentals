f760ff9862027c54d12d47e203fafa2d1d234d33
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-30 14:58:25 +0200

    Update 4.ConvertMeterstoKilometres.js

--- Data Types and Variables - Exercise/4.ConvertMeterstoKilometres.js
+++ Data Types and Variables - Exercise/4.ConvertMeterstoKilometres.js
@@ -1,3 +1,7 @@
+/*
+You will be given a number that will be distance in meters. Write a program that converts meters to kilometers
+formatted to the second decimal point.
+*/
 function solve(num) {
   let converter = num /1000.0
   console.log(converter.toFixed(2))
