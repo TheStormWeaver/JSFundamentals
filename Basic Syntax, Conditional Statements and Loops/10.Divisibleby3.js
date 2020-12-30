49bc96824b1be5db31c60d4366ca15c9a03bb598
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-30 14:56:08 +0200

    Update 10.Divisibleby3.js

--- Basic Syntax, Conditional Statements and Loops/10.Divisibleby3.js
+++ Basic Syntax, Conditional Statements and Loops/10.Divisibleby3.js
@@ -1,3 +1,7 @@
+/*
+Write a program, which prints all the numbers from 1 to 100, which are divisible by 3. You have to use a single for
+loop. The program should not receive input.
+*/
 function solve() {
   for(i=1; i<100; i++){
     if (i % 3 == 0){
