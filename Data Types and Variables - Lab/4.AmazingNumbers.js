f63a621ae54eaa4ae45e866a48302ccb88e8e644
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-31 10:18:41 +0200

    Update 4.AmazingNumbers.js

--- Data Types and Variables - Lab/4.AmazingNumbers.js
+++ Data Types and Variables - Lab/4.AmazingNumbers.js
@@ -1,3 +1,10 @@
+/*
+Write a function which as input will receive a number.
+Check and print if it is amazing or not into the following format:
+"{number} Amazing? {result}";
+An amazing number is one that includes the digit 9 the sum of its digits.
+Examples for amazing numbers are 1233 (1 + 2 + 3 + 3 = 9), 583472 (5 + 8 + 3 + 4 + 7 + 2 = 29)
+*/
 function solve(num) {
   let numasstring = num.toString()
   let sum = 0
