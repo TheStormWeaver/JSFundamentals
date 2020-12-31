9469d8152951c75d77d81a5af71598629b77883f
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-31 10:18:18 +0200

    Update 3.IntegerandFloat.js

--- Data Types and Variables - Lab/3.IntegerandFloat.js
+++ Data Types and Variables - Lab/3.IntegerandFloat.js
@@ -1,3 +1,7 @@
+/*
+You will receive 3 numbers. Your task is to find their sum and print it to the console with the addition
+" - {type of the number (Integer or Float)}":
+*/
 function solve(num1, num2, num3) {
   let sum = num1 + num2 + num3;
   let convertedsum = Math.trunc(sum);
