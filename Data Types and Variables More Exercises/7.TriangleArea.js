21962008e0f24ea17aa8c66da33b620af89c3dfc
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-31 10:22:56 +0200

    Update 7.TriangleArea.js

--- Data Types and Variables More Exercises/7.TriangleArea.js
+++ Data Types and Variables More Exercises/7.TriangleArea.js
@@ -1,3 +1,8 @@
+/*
+Write a function that calculates a triangle’s area by its 3 sides.
+The input comes as three number arguments, representing one side of a triangle.
+The output should be printed to the console.
+*/
 function triangleArea(a, b, c) {
   let s = (a + b + c) / 2;
   let area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
