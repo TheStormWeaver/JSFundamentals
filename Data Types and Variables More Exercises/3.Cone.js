59e01601ecdaeab3aa33eea72f78b195cebc3966
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-31 10:21:15 +0200

    Update 3.Cone.js

--- Data Types and Variables More Exercises/3.Cone.js
+++ Data Types and Variables More Exercises/3.Cone.js
@@ -1,3 +1,9 @@
+/*
+Write a function to calculate a cone’s volume and total surface area by given height and radius at the base.
+The input comes as two number arguments. The first element is the cone’s radius and the second is its height.
+The output should be printed to the console on a new line for every result. The result should be formatted to the
+fourth decimal point
+*/
 function cone(r, h) {
   let volume = (1 / 3) * Math.PI * r ** 2 * h;
   let slant = Math.sqrt(r ** 2 + h ** 2);
