de687df9c4565fd946b2cb130f918cc9e9331f53
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2020-12-31 10:17:05 +0200

    Update 1.ConcatenateNames.js

--- Data Types and Variables - Lab/1.ConcatenateNames.js
+++ Data Types and Variables - Lab/1.ConcatenateNames.js
@@ -1,3 +1,7 @@
+/*
+Write a function which receives two names as string parameters and a delimiter. Print the names joined by
+the delimiter.
+*/
 function solve(firstname, secondname, delimiter) {
   console.log(`${firstname}${delimiter}${secondname}`)
 }
