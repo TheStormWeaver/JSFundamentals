2aef3ca8947ef5ea289044f7b6168a8cc1870cba
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-01 11:13:45 +0200

    Update and rename 3.CharinRangeV2 to 3.CharinRangeV2.js

--- Functions - Exercise/3.CharinRangeV2.js
+++ Functions - Exercise/3.CharinRangeV2.js
@@ -1,3 +1,8 @@
+/*
+Write a function that receives two characters and prints on a single line all the characters in between them
+according to the ASCII code. Keep in mind that the second character code might be before the first one inside the
+ASCII table.
+*/
 function charinRange(a, b) {
   let firstchar = a.charCodeAt();
   let secondchar = b.charCodeAt();
