7df14a4b00cc366fb5148254204405826632984f
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-01 11:18:39 +0200

    Update 1.CarWash.js

--- Functions More Exercises/1.CarWash.js
+++ Functions More Exercises/1.CarWash.js
@@ -1,3 +1,13 @@
+/*
+Write a JS function that receives some commands. Depending on the command add a percentage of how much the
+car is cleaned. Start from 0. The first command will always be "soap":
+ soap – add 10 to the value
+ water – increase the value with 20%
+ vacuum cleaner – increase the value with 25%
+ mud – decrease the value with 10%
+The input comes as an array strings. When finished cleaning the car, print the resulting value in the format:
+"The car is {value}% clean." The value should be rounded to the second decimal point.
+*/
 function carWash(arr) {
   let result = 0;
   for (let i = 0; i < arr.length; i++) {
