c4549c51a19a33341a06c265c31490828f14738b
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-01 11:16:34 +0200

    Update 8.PerfectNumber.js

--- Functions - Exercise/8.PerfectNumber.js
+++ Functions - Exercise/8.PerfectNumber.js
@@ -1,3 +1,8 @@
+/*
+Write a function that receive a number and return if this number is perfect or NOT.
+A perfect number is a positive integer that is equal to the sum of its proper positive divisors. That is the sum of its
+positive divisors excluding the number itself (also known as its aliquot sum).
+*/
 function PerfectNumber(n) {
   let sum = 1
   for (let i = 2; i <= n/2; i++) {    
