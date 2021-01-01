2c8549f509ef98cd27b9ea33e147748f7366b5ab
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-01 11:13:15 +0200

    Update 2.AddandSubtractV2.js

--- Functions - Exercise/2.AddandSubtractV2.js
+++ Functions - Exercise/2.AddandSubtractV2.js
@@ -1,3 +1,8 @@
+/*
+You will receive three integer numbers.
+Write a function sum() to get the sum of the first two integers and subtract() function that subtracts the third
+integer from the result.
+*/
 function AddandSubtract(numOne, numTwo, numThree) {
  function sum(a,b){
    return a + b
