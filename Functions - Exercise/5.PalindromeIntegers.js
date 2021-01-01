7763874033ee88ded36e5a6d1f60145e76a568c5
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-01 11:14:20 +0200

    Update 5.PalindromeIntegers.js

--- Functions - Exercise/5.PalindromeIntegers.js
+++ Functions - Exercise/5.PalindromeIntegers.js
@@ -1,3 +1,7 @@
+/*
+A palindrome is a number which reads the same backward as forward, such as 323 or 1001. Write a function which
+receives an array of positive integer and checks if each integer is a palindrome or not.
+*/
 function polindrome(arr) {
   let result = "";
   for (let num of arr) {
