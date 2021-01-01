b61583d21fe3cfc8b69cca1e962c9339315660f9
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-01 11:16:04 +0200

    Update 6.PasswordValidatorV2.js

--- Functions - Exercise/6.PasswordValidatorV2.js
+++ Functions - Exercise/6.PasswordValidatorV2.js
@@ -1,3 +1,14 @@
+/*
+Write a function that checks if a given password is valid. Password validations are:
+ The length should be 6 - 10 characters (inclusive)
+ It should consists only of letters and digits
+ It should have at least 2 digits
+If a password is valid print "Password is valid".
+If it is NOT valid, for every unfulfilled rule print a message:
+ "Password must be between 6 and 10 characters"
+ "Password must consist only of letters and digits"
+ "Password must have at least 2 digits"
+*/
 function passValidator (password) {
     let checkPassword = (/[\!\@\#\$\%\^\&\*\)\(\+\=\.\<\>\{\}\[\]\:\;\'\"\|\~\`\_\-]/g).test(password);
     let counter = 0;
