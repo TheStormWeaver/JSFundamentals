ea1199e742a07b13c4e9306f934c0f7d65989c39
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-01 11:17:07 +0200

    Update 9.LoadingBarV2.js

--- Functions - Exercise/9.LoadingBarV2.js
+++ Functions - Exercise/9.LoadingBarV2.js
@@ -1,3 +1,8 @@
+/*
+You will receive a single number between 0 and 100 which is divided with 10 without residue (0, 10, 20, 30...).
+Your task is to create a function that visualize a loading bar depending on that number you have received in the
+input.
+*/
 function loadingBar(n) {
   let procentnum = n /10
   let procentloader = ".........."
