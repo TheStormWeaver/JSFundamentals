f93d3944cba21e854018d7e771a9d8472868f8e0
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-02 12:08:16 +0200

    Update 1.Employees.js

--- Objects and Classes Exercise/1.Employees.js
+++ Objects and Classes Exercise/1.Employees.js
@@ -1,3 +1,11 @@
+/*
+You're tasked to create a list of employees and their personal numbers.
+You will receive an array of strings. Each string is an employee name and to assign them a personal number you
+have to find the length of the name (whitespace included).
+Try to use an object.
+At the end print all the list employees in the following format:
+"Name: {employeeName} -- Personal Number: {personalNum}"
+*/
 function employees(arr) {
   let employee = [];
   arr.forEach((element) => {
