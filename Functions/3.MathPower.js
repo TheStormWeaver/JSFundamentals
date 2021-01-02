9f8555f4daff0cb8559b128e91e6c9a2ff50844a
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-02 12:04:16 +0200

    Update 3.MathPower.js

--- Functions/3.MathPower.js
+++ Functions/3.MathPower.js
@@ -1,3 +1,6 @@
+/*
+Write a function that calculates and returns the value of a number raised to a given power:
+*/
 function solve(num, loop) {
   let result = 1
   for (let i = 0; i < loop; i++) {
