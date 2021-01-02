f57ca14c1e340164a71f768336cdd051231e4365
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-02 12:01:16 +0200

    Update 1.RepeatString.js

--- Functions/1.RepeatString.js
+++ Functions/1.RepeatString.js
@@ -1,3 +1,7 @@
+/*
+Write a function that receives a string and a repeat count n. The function should return a new string (the old one
+repeated n times).
+*/
 function solve(string, loop) {
   console.log(`${string.repeat(loop)}`)
 }
