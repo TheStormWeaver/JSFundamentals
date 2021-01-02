e27733e4c44da5376346758d9ff29fd34d48a9b3
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-02 12:05:33 +0200

    Update 6.SignCheck.js

--- Functions/6.SignCheck.js
+++ Functions/6.SignCheck.js
@@ -1,3 +1,9 @@
+/*
+You are given a function, that calculate the result of numOne * numTwo * numThree (the product) is
+negative or positive.
+Try to do this WITHOUT multiplying the 3 numbers.
+The input comes as parameters named numOne, numTwo, numThree.
+*/
 function solve(numOne, numTwo, numThree) {
   let result = "";
   if (numOne >= 0 && numTwo >= 0 && numThree >= 0) {
