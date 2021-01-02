edcb3b5f86544a4d140880615fdc3fc7da680ab0
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-02 12:04:03 +0200

    Update 2.Grades.js

--- Functions/2.Grades.js
+++ Functions/2.Grades.js
@@ -1,3 +1,12 @@
+/*
+Write a function that receives a grade between 2.00 and 6.00 and prints a formatted line with grade and
+description
+ < 3.00 - "Fail"
+ >= 3.00 and < 3.50 - "Poor"
+ >= 3.50 and < 4.50 - "Good"
+ >= 4.50 and < 5.50 - "Very good"
+ >= 5.50 - "Excellent"
+*/
 function solve(grade) {
   if(grade < 3.00){
     console.log(`Fail (2)`)
