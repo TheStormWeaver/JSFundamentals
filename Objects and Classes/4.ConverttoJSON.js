1b074e14ba32de39b79929bb592308dd4f0c7920
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-03 15:55:23 +0200

    Update 4.ConverttoJSON.js

--- Objects and Classes/4.ConverttoJSON.js
+++ Objects and Classes/4.ConverttoJSON.js
@@ -1,3 +1,8 @@
+/*
+Write a Function That Receives Name, LastName, HairColor and Sets Them to an Object.
+Convert the object to JSON string and print it.
+Input is provided as 3 single strings in the order stated above.
+*/
 function convertToJSON(name, lastName, hairColor) {
   let person = {
     name,
