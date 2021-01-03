2340699ecbf1b24689dbc85bdb855863649383f9
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-03 15:54:20 +0200

    Update 1.PersonInfo.js

--- Objects and Classes/1.PersonInfo.js
+++ Objects and Classes/1.PersonInfo.js
@@ -1,3 +1,7 @@
+/*
+Write a function that receives 3 parameters, sets them to an object and returns that object.
+The input comes as 3 separate strings in the following order: firstName, lastName, age.
+*/
 function personalInfo(arr) {
   let firstName = arr.shift()
   let lastName = arr.shift()
