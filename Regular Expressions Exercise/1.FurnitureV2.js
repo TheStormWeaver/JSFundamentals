a82b0dbaccc5820eb12daaa3ce7520d55597e3be
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-04 11:56:37 +0200

    Update 1.FurnitureV2.js

--- Regular Expressions Exercise/1.FurnitureV2.js
+++ Regular Expressions Exercise/1.FurnitureV2.js
@@ -1,7 +1,7 @@
 /*
 Write a function to calculate the total cost of different types of furniture. You will be given some lines of input until
 you receive the line &quot;Purchase&quot;. For the line to be valid it should be in the following format:
-&quot;&gt;&gt;{furniture name}&lt;&lt;{price}!{quantity}&quot;
+">>{furniture name}<<{price}!{quantity}"
 The price can be floating point number or whole number. Store the names of the furniture and the total price. At the
 end print the each bought furniture on separate line in the format:
 "Bought furniture:
