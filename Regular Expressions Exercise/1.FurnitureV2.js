6b8cb0aaa92f1facae71f27af7074c0afaaa57f0
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-04 11:55:58 +0200

    Update 1.FurnitureV2.js

--- Regular Expressions Exercise/1.FurnitureV2.js
+++ Regular Expressions Exercise/1.FurnitureV2.js
@@ -1,3 +1,16 @@
+/*
+Write a function to calculate the total cost of different types of furniture. You will be given some lines of input until
+you receive the line &quot;Purchase&quot;. For the line to be valid it should be in the following format:
+&quot;&gt;&gt;{furniture name}&lt;&lt;{price}!{quantity}&quot;
+The price can be floating point number or whole number. Store the names of the furniture and the total price. At the
+end print the each bought furniture on separate line in the format:
+"Bought furniture:
+{1 st name}
+{2 nd name}
+…"
+And on the last line print the following: "Total money spend: {spend money}" formatted to the second decimal
+point.
+*/
 function furniture(arr) {
   let result = `Bought furniture:\n`;
   let regex = />>(?<name>\w+)<<(?<price>[\d.\d]+)!(?<quantity>\d+)/;
