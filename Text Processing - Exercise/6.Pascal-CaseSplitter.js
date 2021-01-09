473c697a23dbc0325479a739c7d761966ef10c07
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-09 10:27:25 +0200

    Update 6.Pascal-CaseSplitter.js

--- Text Processing - Exercise/6.Pascal-CaseSplitter.js
+++ Text Processing - Exercise/6.Pascal-CaseSplitter.js
@@ -1,3 +1,8 @@
+/*
+You will receive a single string. 
+This string is written in PascalCase format. Your task here is to split this string by every word in it. 
+Print them joined by comma and space.
+*/
 function pascalCaseSplitter(str) {
   let capitalIndex = 0
   let result = []
