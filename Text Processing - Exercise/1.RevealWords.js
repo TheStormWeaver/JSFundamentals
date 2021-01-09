a5462c15126de3ad8180a58482ab4c8c9c13fe72
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-09 10:25:57 +0200

    Update 1.RevealWords.js

--- Text Processing - Exercise/1.RevealWords.js
+++ Text Processing - Exercise/1.RevealWords.js
@@ -1,3 +1,9 @@
+/*
+Write a function, which receives two parameters. 
+The first parameter will be a string with some words separated by ', '.
+The second parameter will be a string which contains templates containing '*'.
+Find the word with the exact same length as the template and replace it.
+*/
 function revealWords(words, text) {
   words = words.split(", ");
 
