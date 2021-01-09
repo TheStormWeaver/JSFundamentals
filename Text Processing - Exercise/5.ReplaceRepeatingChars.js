44761eb98937ef504ef720e44ea9fef4da7d24c9
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-09 10:27:06 +0200

    Update 5.ReplaceRepeatingChars.js

--- Text Processing - Exercise/5.ReplaceRepeatingChars.js
+++ Text Processing - Exercise/5.ReplaceRepeatingChars.js
@@ -1,3 +1,6 @@
+/*
+Write a function that receives a single string and replaces any sequence of the same letters with a single corresponding letter.
+*/
 function replaceRepeatingChars(str) {
   result = ""
   for (let i = 0; i < str.length; i++) {
