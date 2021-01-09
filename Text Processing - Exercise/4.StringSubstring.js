197217dfd00ef985bf0871b841a09011b902eb47
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-09 10:26:50 +0200

    Update 4.StringSubstring.js

--- Text Processing - Exercise/4.StringSubstring.js
+++ Text Processing - Exercise/4.StringSubstring.js
@@ -1,3 +1,8 @@
+/*
+The input will be given as two separated strings.
+Write a function that checks given text for containing a given word. The comparison should be case insensitive. Once you find match, print the word and stop the program. 
+If you don't find the word print "{word} not found!"
+*/
 function strSubstring(word, text) {
   text = text.toLowerCase().split(" ")
   if(text.includes(word.toLowerCase())){
