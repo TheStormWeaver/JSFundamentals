acbc76617d19a526b5b999be16644428287a8ba9
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-09-24 11:26:21 +0300

    Add files via upload

---
+++ Basic Web Project - Phonebook/controllers/phonebook-controller.js
@@ -0,0 +1,14 @@
+const phonebook = require('../phonebook');
+
+module.exports = {
+  index: (req, res) => {
+    let contacts = phonebook.getAll()
+    res.render('index', {contacts});
+  },
+  addPhonebookPost:(req, res) => {
+    let name = req.body.name
+    let number = req.body.number
+    phonebook.addContact(name, number)
+    res.redirect("/")
+  }
+}
